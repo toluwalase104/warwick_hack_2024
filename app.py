401988b094887c9014841e1dce422d1e924f1940
JonathanLu2005 <jonathanlu2005@yahoo.com>
2024-10-26 13:17:11 +0100

    Testing

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify 
 
 app = Flask(__name__)
 
-@app.route('/')
+# Home page
+@app.route("/", methods=["POST", "GET"])
 def home():
-    return 'Hello, Flask!'
+    return render_template("home.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Sending data page
+@app.route("/resourceRequests", methods=["POST", "GET"])
+def resourceRequests():
+    return render_template("resourceRequests.html")
+
+# Live tracker
+@app.route("/liveTracker", methods=["POST", "GET"])
+def liveTracker():
+    return render_template("liveTracker.html")
+
+# Charities to donate
+@app.route("/charities", methods=["POST", "GET"])
+def charities():
+    return render_template("charities.html")
+
